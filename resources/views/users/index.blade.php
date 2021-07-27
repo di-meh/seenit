80e9fa7e608c01ccc645826edcc42ae3f288c06b
Mehdi Saber <m1d3s@protonmail.com>
2021-07-27 22:46:08 +0200

    Fixed subseenits undefined

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -44,14 +44,14 @@
                                         @if(Auth::check())
                                             <div class="flex items-center space-x-2">
                                                 @if(Auth::id() == $post->user_id)
-                                                    <a href="{{route('subseenits.posts.edit', [$subseenit, $post])}}">
+                                                    <a href="{{route('subseenits.posts.edit', [$post->subseenit, $post])}}">
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                                         </svg>
                                                     </a>
                                                 @endif
-                                                @if (Auth::id() == $subseenit->user_id || Auth::user()->is_admin || Auth::id() == $post->user_id)
-                                                    <form method="POST" action="{{route('subseenits.posts.destroy', [$subseenit, $post] )}}" class="hover:text-red-500">
+                                                @if (Auth::id() == $post->subseenit->user_id || Auth::user()->is_admin || Auth::id() == $post->user_id)
+                                                    <form method="POST" action="{{route('subseenits.posts.destroy', [$post->subseenit, $post] )}}" class="hover:text-red-500">
                                                         @csrf
 
                                                         <button type="submit">
